de7bb8fba8723f89467ae9f42cc7d9b6c9c9fa8d
Guy Or <28857112+guyo13@users.noreply.github.com>
2020-01-13 17:17:26 +0200

    Clean js code repetition progress HTML

--- javascript/fmt.js
+++ javascript/fmt.js
@@ -1,6 +1,5 @@
-$(document).ready(function() {
-    let tabIds = ["goto-overview","goto-foods", "goto-profile", "goto-export"];
-    let pageController = {
+    var tabIds = ["goto-overview","goto-foods", "goto-profile", "goto-export", "goto-import"];
+    var pageController = {
         hasLocalStorage: function() {
             try {
                 return 'localStorage' in window && window['localStorage'] !== null;
@@ -15,43 +14,25 @@ $(document).ready(function() {
             for (const i in tabIds) {
                 let s = "#" + tabIds[i];
                 $(s).removeClass("active");
+                let areaToHideName = "#" + tabIds[i].split("-")[1];
+                $(areaToHideName).hide();
             }
             let active = "#" + tabName;
             $(active).addClass("active");
+            let areaName = "#" + tabName.split("-")[1];
+            $(areaName).show();
         },
-        showOverview: function () {
-            pageController.setTabActive("goto-overview");
-            $("#export").hide();
-            $("#profile").hide();
-            $("#foods").hide();
-            $("#overview").show();
-        },
-        showFoods: function () {
-            pageController.setTabActive("goto-foods");
-            $("#overview").hide();
-            $("#profile").hide();
-            $("#export").hide();
-            $("#foods").show();
-        },
-        showExport: function () {
-            pageController.setTabActive("goto-export");
-            $("#overview").hide();
-            $("#profile").hide();
-            $("#foods").hide();
-            $("#export").show();
-        },
-        showProfile: function () {
-            pageController.setTabActive("goto-profile");
-            $("#overview").hide();
-            $("#foods").hide();
-            $("#export").hide();
-            $("#profile").show();
-        },
+        showOverview: function () {pageController.setTabActive("goto-overview");},
+        showFoods: function () {pageController.setTabActive("goto-foods");},
+        showExport: function () {pageController.setTabActive("goto-export");},
+        showProfile: function () {pageController.setTabActive("goto-profile");},
     };
-    $("#goto-foods").click(pageController.showFoods);
-    $("#goto-overview").click(pageController.showOverview);
-    $("#goto-export").click(pageController.showExport);
-    $("#goto-profile").click(pageController.showProfile);
+
+$(document).ready(function() {
+    for (const i in tabIds) {
+        let tabId = tabIds[i];
+        $("#" + tabId).click(function () {pageController.setTabActive(tabId)});
+    }
     pageController.showOverview();
     let hasLocalStorage = pageController.hasLocalStorage();
     if (hasLocalStorage) {
