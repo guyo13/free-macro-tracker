dcb5dfca81bf2455b641f70dc9901b6f05f31e3a
Guy Or <28857112+guyo13@users.noreply.github.com>
2020-01-14 09:32:44 +0200

    Indent js code

--- javascript/fmt.js
+++ javascript/fmt.js
@@ -1,33 +1,36 @@
-    var tabIds = ["goto-overview","goto-foods", "goto-profile", "goto-export", "goto-import"];
-    var pageController = {
-        hasLocalStorage: function() {
-            try {
-                return 'localStorage' in window && window['localStorage'] !== null;
-            } catch(e) {
-                return false;
-            }
-        },
-        setTabActive: function(tabName) {
-            if (tabIds.indexOf(tabName) <0 ) {
-                return;
-            }
-            for (const i in tabIds) {
-                let s = "#" + tabIds[i];
-                $(s).removeClass("active");
-                let areaToHideName = "#" + tabIds[i].split("-")[1];
-                $(areaToHideName).hide();
-            }
-            let active = "#" + tabName;
-            $(active).addClass("active");
-            let areaName = "#" + tabName.split("-")[1];
-            $(areaName).show();
-        },
-        showOverview: function () {pageController.setTabActive("goto-overview");},
-        showFoods: function () {pageController.setTabActive("goto-foods");},
-        showExport: function () {pageController.setTabActive("goto-export");},
-        showProfile: function () {pageController.setTabActive("goto-profile");},
-    };
+//Classes
 
+//Page
+var tabIds = ["goto-overview","goto-foods", "goto-profile", "goto-export", "goto-import"];
+var pageController = {
+    hasLocalStorage: function() {
+        try {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        } catch(e) {
+            return false;
+        }
+    },
+    setTabActive: function(tabName) {
+        if (tabIds.indexOf(tabName) <0 ) {
+            return;
+        }
+        for (const i in tabIds) {
+            let s = "#" + tabIds[i];
+            $(s).removeClass("active");
+            let areaToHideName = "#" + tabIds[i].split("-")[1];
+            $(areaToHideName).hide();
+        }
+        let active = "#" + tabName;
+        $(active).addClass("active");
+        let areaName = "#" + tabName.split("-")[1];
+        $(areaName).show();
+    },
+    showOverview: function () {pageController.setTabActive("goto-overview");},
+    showFoods: function () {pageController.setTabActive("goto-foods");},
+    showExport: function () {pageController.setTabActive("goto-export");},
+    showProfile: function () {pageController.setTabActive("goto-profile");},
+};
+//Main
 $(document).ready(function() {
     for (const i in tabIds) {
         let tabId = tabIds[i];
