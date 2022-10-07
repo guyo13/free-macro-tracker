c4d9a118d5a7e51741fe79bde4de06b998812678
Guy Or <guydude1@gmail.com>
2022-10-07 16:19:18 +0300

    Add function to validate consumable

--- src/models/consumable.ts
+++ src/models/consumable.ts
@@ -1,3 +1,4 @@
+import { isPositiveNumber } from "../utils/utils";
 import type { INutritionalValue } from "./nutrient";
 import type IDBRecord from "./record";
 
@@ -5,6 +6,28 @@ export enum ConsumableType {
   // TODO - Add compatibility with previous "Food/Recipe Item" enum values
   Food = 1,
   Recipe = 2,
+}
+
+export function validateConsumable(
+  name: any,
+  _brand: any,
+  referenceServing: any,
+  units: any,
+  nutritionalValue: any
+) {
+  if (!name) {
+    throw "Name must not be empty.";
+  }
+  if (!isPositiveNumber(referenceServing)) {
+    throw `Reference serving must be positive number. Got '${referenceServing}'`;
+  }
+  if (!units) {
+    throw "Units must be specified.";
+  }
+  if (!nutritionalValue) {
+    throw "Nutritional value must not be empty.";
+  }
+  // TODO - Validate nutritional value!
 }
 
 export default interface IConsumable extends IDBRecord {
