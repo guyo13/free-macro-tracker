bbec85e145b9604bd55c868acb11abb705bb73b7
Guy Or <guydude1@gmail.com>
2022-10-10 11:20:37 +0300

    Format

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,75 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
-const path = require('path');
-const sveltePreprocess = require('svelte-preprocess');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const path = require("path");
+const sveltePreprocess = require("svelte-preprocess");
 
-const mode = process.env.NODE_ENV || 'development';
-const prod = mode === 'production';
-console.log('mode', mode);
+const mode = process.env.NODE_ENV || "development";
+const prod = mode === "production";
+console.log("mode", mode);
 module.exports = {
-	entry: {
-		'app':  {
-			import: ['./src/main.ts'],
-		},
-	},
-	resolve: {
-		alias: {
-			svelte: path.dirname(require.resolve('svelte/package.json'))
-		},
-		extensions: ['.mjs', '.js', '.ts', '.svelte'],
-		mainFields: ['svelte', 'browser', 'module', 'main']
-	},
-	output: {
-		path: path.join(__dirname, 'public', 'build'),
-		filename: '[name].bundle.js',
-		chunkFilename: '[name].[id].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts/,
-				loader: 'ts-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.svelte$/,
-				use: {
-					loader: 'svelte-loader',
-					options: {
-						compilerOptions: {
-							dev: !prod
-						},
-						emitCss: prod,
-						hotReload: !prod,
-						preprocess: sveltePreprocess({ sourceMap: !prod })
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader'
-				]
-			},
-			{
-				// required to prevent errors from Svelte on Webpack 5+
-				test: /node_modules\/svelte\/.*\.mjs$/,
-				resolve: {
-					fullySpecified: false
-				}
-			},
-		]
-	},
-	mode,
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		}),
-		new ESLintPlugin(),
-	],
-	devtool: prod ? false : 'source-map',
-	devServer: {
-		hot: true,
-		devMiddleware: {
-			publicPath: '/build'
-		}
-	}
+  entry: {
+    app: {
+      import: ["./src/main.ts"],
+    },
+  },
+  resolve: {
+    alias: {
+      svelte: path.dirname(require.resolve("svelte/package.json")),
+    },
+    extensions: [".mjs", ".js", ".ts", ".svelte"],
+    mainFields: ["svelte", "browser", "module", "main"],
+  },
+  output: {
+    path: path.join(__dirname, "public", "build"),
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].[id].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svelte$/,
+        use: {
+          loader: "svelte-loader",
+          options: {
+            compilerOptions: {
+              dev: !prod,
+            },
+            emitCss: prod,
+            hotReload: !prod,
+            preprocess: sveltePreprocess({ sourceMap: !prod }),
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        // required to prevent errors from Svelte on Webpack 5+
+        test: /node_modules\/svelte\/.*\.mjs$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+  mode,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new ESLintPlugin(),
+  ],
+  devtool: prod ? false : "source-map",
+  devServer: {
+    hot: true,
+    devMiddleware: {
+      publicPath: "/build",
+    },
+  },
 };
