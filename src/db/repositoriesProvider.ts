2f9dc6d25befab5c604110f8295a0c2c8e56d50d
Guy Or <guydude1@gmail.com>
2022-10-18 21:25:57 +0300

    Add repositories provider utility that converts a list of repository stores to a store of initialized repositories

---
+++ src/db/repositoriesProvider.ts
@@ -0,0 +1,51 @@
+// Copyright (c) 2020-2022, Guy Or Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a GNU GPL
+// license that can be found in the LICENSE file.
+
+import type IDBWrapper from "idb_wrapper.js";
+import { derived, type Readable } from "svelte/store";
+import idbConnector from "./idb";
+import type { IRepository } from "./repository";
+
+type RepositoryProviders = Readable<IRepository>[];
+
+const repositoriesProvider: IRepositoriesProvider = {
+  synced: <S extends RepositoryProviders>(
+    providers: S
+  ): Readable<IRepository[]> => {
+    let initializedCount = 0;
+    let isIntialized = false;
+
+    return derived<
+      [Readable<IDBWrapper>, ...Readable<IRepository>[]],
+      IRepository[]
+    >([idbConnector, ...providers], ([connector, ...repositories], set) => {
+      for (const repo of repositories) {
+        if (repo) {
+          initializedCount++;
+        }
+      }
+
+      const areReposInitialized = initializedCount === providers.length;
+
+      if (areReposInitialized && !isIntialized) {
+        isIntialized = true;
+        connector
+          .wait()
+          .then(() => {
+            set([...repositories]);
+          })
+          .catch((err) =>
+            console.error("Error while loading repositories.", err)
+          );
+      }
+    });
+  },
+};
+
+export interface IRepositoriesProvider {
+  synced: <S extends RepositoryProviders>(
+    providers: S
+  ) => Readable<IRepository[]>;
+}
+export default repositoriesProvider;
