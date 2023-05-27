5eafd006c947cb0c9f72a056dd60f4798cf6414e
Guy Or <guydude1@gmail.com>
2023-05-27 19:50:00 +0300

    export service refactor as class and extract common cursor handling logic to private method

--- src/db/exportService.ts
+++ src/db/exportService.ts
@@ -2,21 +2,28 @@
 // All rights reserved. Use of this source code is governed by a GNU GPL
 // license that can be found in the LICENSE file.
 
+import { type IDBCursorWithTypedValue } from "idb_wrapper.js";
 import type { IUnit } from "../models/units";
 import type { IUnitRepository } from "./unitRepository";
 
-const exportService: IExportService = {
-  exportUnits: (
-    unitRepository: IUnitRepository,
-    writer: CursorConsumer<IUnit>
-  ) => {
+class ExportService implements IExportService {
+  readonly #unitRepository: IUnitRepository;
+
+  constructor(unitRepository: IUnitRepository) {
+    this.#unitRepository = unitRepository;
+  }
+
+  #export<T>(
+    getCursor: () => Promise<IDBCursorWithTypedValue<T>>,
+    writer: CursorConsumer<T>
+  ): Promise<void> {
     return new Promise(async (resolve, reject) => {
       try {
-        const unitCursor = await unitRepository.interateUnits();
-        if (unitCursor) {
-          const unit = unitCursor.value;
-          writer.consume(unit);
-          unitCursor.continue();
+        const cursor = await getCursor();
+        if (cursor) {
+          const document = cursor.value;
+          writer.consume(document);
+          cursor.continue();
         } else {
           resolve();
         }
@@ -24,8 +31,12 @@ const exportService: IExportService = {
         reject(err);
       }
     });
-  },
-};
+  }
+
+  exportUnits(writer: CursorConsumer<IUnit>): Promise<void> {
+    return this.#export(this.#unitRepository.interateUnits, writer);
+  }
+}
 
 export interface CursorConsumer<T> {
   consume: (arg: T) => void;
@@ -40,10 +51,7 @@ export interface IExportService {
   //   records[fmtAppGlobals.FMT_DB_MEAL_ENTRIES_STORE] = {};
   //   records[fmtAppGlobals.FMT_DB_USER_GOALS_STORE] = {};
   //   records[fmtAppGlobals.FMT_DB_PROFILES_STORE] = [];
-  exportUnits: (
-    unitRepository: IUnitRepository,
-    writer: CursorConsumer<IUnit>
-  ) => Promise<void>;
+  exportUnits: (writer: CursorConsumer<IUnit>) => Promise<void>;
 }
 
-export default exportService;
+export default ExportService;
