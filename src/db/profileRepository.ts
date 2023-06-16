cd6a52502916466eef0becf097eb2365cb795ba8
Guy Or <guydude1@gmail.com>
2023-06-16 13:21:18 +0300

    Refactor methods in profile repository

--- src/db/profileRepository.ts
+++ src/db/profileRepository.ts
@@ -18,7 +18,7 @@ import UserProfile from "../models/userProfile";
 const FMT_DB_PROFILES_STORE = "fmt_profiles";
 
 class ProfileRepository extends Repository implements IProfileRepository {
-  async iterateProfiles(): Promise<IDBCursorWithTypedValue<IUserProfile>> {
+  iterateProfiles(): Promise<IDBCursorWithTypedValue<IUserProfile>> {
     return this.iterate<IUserProfile>(IDBTransactionModes.Readonly);
   }
 
@@ -26,93 +26,30 @@ class ProfileRepository extends Repository implements IProfileRepository {
     if (!this.isReady) {
       await this.connection.wait();
     }
-    return new Promise((resolve, reject) => {
-      const profileStore = this.connection.getObjectStore(
-        this.storeName,
-        IDBTransactionModes.Readonly
-      );
-      const getAllRequest = profileStore.getAll();
-      getAllRequest.onsuccess = (ev: Event) => {
-        // @ts-ignore
-        const profiles: any[] | undefined = ev?.target?.result;
-        resolve(
-          profiles
-            ? profiles.map((profile) => UserProfile.fromObject(profile))
-            : []
-        );
-      };
-      getAllRequest.onerror = (_ev: Event) => {
-        reject("Failed getting all profiles");
-      };
-    });
+    return (await this.connection.getAll<IUserProfile>(this.storeName))?.map(
+      (profile) => UserProfile.fromObject(profile)
+    );
   }
 
   async getProfile(profileId: RecordId): Promise<IUserProfile | null> {
     if (!this.isReady) {
       await this.connection.wait();
     }
-    return new Promise((resolve, reject) => {
-      const profileStore = this.connection.getObjectStore(
-        this.storeName,
-        IDBTransactionModes.Readonly
-      );
-      const getRequest = profileStore.get(profileId);
-      getRequest.onsuccess = (ev: Event) => {
-        //@ts-ignore
-        const result: any | undefined = ev?.target?.result;
-        try {
-          const profile = result ? UserProfile.fromObject(result) : null;
-          resolve(profile);
-        } catch (err) {
-          reject(err);
-        }
-      };
-      getRequest.onerror = (_ev: Event) => {
-        reject(`Failed getting profile with id '${profileId}`);
-      };
-    });
+    const profile = await this.connection.get<IUserProfile>(
+      this.storeName,
+      profileId
+    );
+    return profile && UserProfile.fromObject(profile);
   }
 
-  async addProfile(profile: IUserProfile): Promise<void> {
-    if (!this.isReady) {
-      await this.connection.wait();
-    }
-    return new Promise((resolve, reject) => {
-      const profileStore = this.connection.getObjectStore(
-        this.storeName,
-        IDBTransactionModes.Readwrite
-      );
-      const dateModified = new Date();
-      const userProfile = UserProfile.fromObject(profile, dateModified);
-      const addRequest = profileStore.add(userProfile);
-      addRequest.onsuccess = (_ev: Event) => {
-        resolve();
-      };
-      addRequest.onerror = (_ev: Event) => {
-        reject(`Failed adding profile ${JSON.stringify(userProfile)}`);
-      };
-    });
+  addProfile(profile: IUserProfile): Promise<void> {
+    const userProfile = UserProfile.fromObject(profile, new Date());
+    return this.add(userProfile);
   }
 
-  async updateProfile(profile: IUserProfile): Promise<void> {
-    if (!this.isReady) {
-      await this.connection.wait();
-    }
-    return new Promise((resolve, reject) => {
-      const profileStore = this.connection.getObjectStore(
-        this.storeName,
-        IDBTransactionModes.Readwrite
-      );
-      const dateModified = new Date();
-      const userProfile = UserProfile.fromObject(profile, dateModified);
-      const putRequest = profileStore.put(userProfile);
-      putRequest.onsuccess = (_ev: Event) => {
-        resolve();
-      };
-      putRequest.onerror = (_ev: Event) => {
-        reject(`Failed updating profile ${JSON.stringify(userProfile)}`);
-      };
-    });
+  updateProfile(profile: IUserProfile): Promise<void> {
+    const userProfile = UserProfile.fromObject(profile, new Date());
+    return this.update(userProfile);
   }
 
   async deleteProfile(profileId: RecordId): Promise<void> {
