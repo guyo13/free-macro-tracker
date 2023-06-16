11af0b445325f59f5c005b444bd378e4087cdfa7
Guy Or <guydude1@gmail.com>
2023-06-16 18:21:18 +0300

    wip export service

--- src/db/exportService.ts
+++ src/db/exportService.ts
@@ -2,7 +2,7 @@
 // All rights reserved. Use of this source code is governed by a GNU GPL
 // license that can be found in the LICENSE file.
 
-import { type IDBCursorWithTypedValue } from "idb_wrapper.js";
+import type { CursorConsumer } from "idb_wrapper.js";
 import type { IUnit } from "../models/units";
 import type { IUnitRepository } from "./unitRepository";
 import type { INutrientRepository } from "./nutrientRepository";
@@ -11,64 +11,49 @@ import type { IUserProfile } from "../models/userProfile";
 import type { IProfileRepository } from "./profileRepository";
 import type { IFood } from "../models/food";
 import type { IFoodRepository } from "./foodRepository";
+import type { IUserGoals } from "../models/userGoals";
+import type { IUserGoalsRepository } from "./userGoalsRepository";
 
 class ExportService implements IExportService {
   readonly #unitRepository: IUnitRepository;
   readonly #nutrientRepository: INutrientRepository;
   readonly #profileRepository: IProfileRepository;
   readonly #foodRepository: IFoodRepository;
+  readonly #userGoalsRepository: IUserGoalsRepository;
 
   constructor(
     unitRepository: IUnitRepository,
     nutrientRepository: INutrientRepository,
     profileRepository: IProfileRepository,
-    foodRepository: IFoodRepository
+    foodRepository: IFoodRepository,
+    userGoalsRepository: IUserGoalsRepository
   ) {
     this.#unitRepository = unitRepository;
     this.#nutrientRepository = nutrientRepository;
     this.#profileRepository = profileRepository;
     this.#foodRepository = foodRepository;
-  }
-
-  #export<T>(
-    getCursor: () => Promise<IDBCursorWithTypedValue<T>>,
-    writer: CursorConsumer<T>
-  ): Promise<void> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const cursor = await getCursor();
-        if (cursor) {
-          const document = cursor.value;
-          writer.consume(document);
-          cursor.continue();
-        } else {
-          resolve();
-        }
-      } catch (err) {
-        reject(err);
-      }
-    });
+    this.#userGoalsRepository = userGoalsRepository;
   }
 
   exportUnits(writer: CursorConsumer<IUnit>): Promise<void> {
-    return this.#export(this.#unitRepository.iterateUnits, writer);
+    return this.#unitRepository.iterateUnits(writer);
   }
 
   exportNutrients(writer: CursorConsumer<INutrientDefinition>): Promise<void> {
-    return this.#export(this.#nutrientRepository.iterateNutrients, writer);
+    return this.#nutrientRepository.iterateNutrients(writer);
   }
 
   exportProfiles(writer: CursorConsumer<IUserProfile>): Promise<void> {
-    return this.#export(this.#profileRepository.iterateProfiles, writer);
+    return this.#profileRepository.iterateProfiles(writer);
   }
 
   exportFoods(writer: CursorConsumer<IFood>): Promise<void> {
-    return this.#export(this.#foodRepository.iterateFoods, writer);
+    return this.#foodRepository.iterateFoods(writer);
   }
-}
 
-export interface CursorConsumer<T> {
-  consume: (arg: T) => void;
+  exportUserGoals(writer: CursorConsumer<IUserGoals>): Promise<void> {
+    return this.#userGoalsRepository.iterateUserGoals(writer);
+  }
 }
 
 export interface IExportService {
@@ -82,6 +67,7 @@ export interface IExportService {
   ) => Promise<void>;
   exportProfiles: (writer: CursorConsumer<IUserProfile>) => Promise<void>;
   exportFoods: (writer: CursorConsumer<IFood>) => Promise<void>;
+  exportUserGoals: (writer: CursorConsumer<IUserGoals>) => Promise<void>;
 }
 
 export default ExportService;
