778cc480c7531820ac0680edd9cc925d1c755d35
Guy Or <guydude1@gmail.com>
2023-06-16 13:24:13 +0300

    Refactor methods in unit repository

--- src/db/unitRepository.ts
+++ src/db/unitRepository.ts
@@ -17,7 +17,7 @@ import Unit from "../models/units";
 const FMT_DB_UNITS_STORE = "fmt_units";
 
 class UnitRepository extends Repository implements IUnitRepository {
-  async iterateUnits(): Promise<IDBCursorWithTypedValue<IUnit>> {
+  iterateUnits(): Promise<IDBCursorWithTypedValue<IUnit>> {
     return this.iterate<IUnit>(IDBTransactionModes.Readonly);
   }
 
@@ -25,85 +25,25 @@ class UnitRepository extends Repository implements IUnitRepository {
     if (!this.isReady) {
       await this.connection.wait();
     }
-    return new Promise((resolve, reject) => {
-      const unitStore = this.connection.getObjectStore(
-        this.storeName,
-        IDBTransactionModes.Readonly
-      );
-      const getAllRequest = unitStore.getAll();
-      getAllRequest.onsuccess = (ev: Event) => {
-        // @ts-ignore
-        const units: any[] | undefined = ev?.target?.result;
-        resolve(units ? units.map(Unit.fromObject) : []);
-      };
-      getAllRequest.onerror = (_ev: Event) => {
-        reject("Failed getting all units");
-      };
-    });
+    return (await this.connection.getAll<IUnit>(this.storeName))?.map(
+      Unit.fromObject
+    );
   }
 
   async getUnit(unitName: string): Promise<IUnit | null> {
     if (!this.isReady) {
       await this.connection.wait();
     }
-    return new Promise((resolve, reject) => {
-      const unitStore = this.connection.getObjectStore(
-        this.storeName,
-        IDBTransactionModes.Readonly
-      );
-      const getRequest = unitStore.get(unitName);
-      getRequest.onsuccess = (ev: Event) => {
-        // @ts-ignore
-        const result: any | undefined = ev?.target?.result;
-        try {
-          const unit = result ? Unit.fromObject(result) : null;
-          resolve(unit);
-        } catch (err) {
-          reject(err);
-        }
-      };
-      getRequest.onerror = (_ev: Event) => {
-        reject(`Failed getting unit with name '${unitName}`);
-      };
-    });
+    const unit = await this.connection.get<IUnit>(this.storeName, unitName);
+    return unit && Unit.fromObject(unit);
   }
 
-  async addUnit(unit: IUnit): Promise<void> {
-    if (!this.isReady) {
-      await this.connection.wait();
-    }
-    return new Promise((resolve, reject) => {
-      const unitStore = this.connection.getObjectStore(
-        this.storeName,
-        IDBTransactionModes.Readwrite
-      );
-      const addRequest = unitStore.add(unit);
-      addRequest.onsuccess = (_ev: Event) => {
-        resolve();
-      };
-      addRequest.onerror = (_ev: Event) => {
-        reject(`Failed adding unit ${JSON.stringify(unit)}`);
-      };
-    });
+  addUnit(unit: IUnit): Promise<void> {
+    return this.add(unit);
   }
 
-  async updateUnit(unit: IUnit): Promise<void> {
-    if (!this.isReady) {
-      await this.connection.wait();
-    }
-    return new Promise((resolve, reject) => {
-      const unitStore = this.connection.getObjectStore(
-        this.storeName,
-        IDBTransactionModes.Readwrite
-      );
-      const putRequest = unitStore.put(unit);
-      putRequest.onsuccess = (_ev: Event) => {
-        resolve();
-      };
-      putRequest.onerror = (_ev: Event) => {
-        reject(`Failed updating unit ${JSON.stringify(unit)}`);
-      };
-    });
+  updateUnit(unit: IUnit): Promise<void> {
+    return this.update(unit);
   }
 
   async deleteUnit(unitName: string): Promise<void> {
