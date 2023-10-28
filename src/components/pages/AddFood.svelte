e7e3e7d163cff5c609ef3de7e6fbaa0f31c3b116
Guy Or <guydude1@gmail.com>
2023-10-28 19:53:01 +0300

    wip refactoring AddFood

--- src/components/pages/AddFood.svelte
+++ src/components/pages/AddFood.svelte
@@ -22,11 +22,23 @@ license that can be found in the LICENSE file. -->
     fats: "Fats",
     micronutrients: "Micronutrients",
   };
+
+  let name: string, brand: string, serving: string, calories: string, proteins: string, carbs: string, fats: string;
+  let isShowingMicros = false;
+  $: expandCollapseText = isShowingMicros ? TEXT.showLess : TEXT.showMore;
+
+  function handleSave() {
+    console.log({ name, brand, serving, calories, proteins, carbs, fats });
+  }
+
+  function handleExpandOrCollapse() {
+    isShowingMicros = !isShowingMicros;
+  }
 </script>
 
 <div id="add-food-screen" class="fmt-dynamic-screen container-fluid">
   <div id="add-food-screen-alerts" class="row justify-content-center" />
-  <div class="container max-w-5xl">
+  <div class="container flex flex-col max-w-5xl h-full">
     <!-- Heading -->
     <div class="flex mb-3 justify-between mt-3">
       <span class="fmt-font-2">Add Food</span>
@@ -35,35 +47,36 @@ license that can be found in the LICENSE file. -->
     <!-- Content -->
     <div class="flex flex-col overflow-y-auto overflow-x-hidden">
       <Label for="add-food-screen-food-name">{TEXT.name}</Label>
-      <Input id="add-food-screen-food-name" size="sm" placeholder="{TEXT.name}" required />
+      <Input id="add-food-screen-food-name" size="sm" placeholder="{TEXT.name}" required bind:value={name} />
       <Label for="add-food-screen-food-brand">{TEXT.brand}</Label>
-      <Input id="add-food-screen-food-brand" size="sm" placeholder="{TEXT.brand}" required />
+      <Input id="add-food-screen-food-brand" size="sm" placeholder="{TEXT.brand}" required bind:value={brand} />
       <Label for="add-food-screen-food-serving-input">{TEXT.serving}</Label>
       <Input id="add-food-screen-food-serving-input" size="sm" placeholder="{TEXT.serving}" type="number" step="any"
-             required />
+             required bind:value={serving} />
       <h4 class="text-2xl font-light mt-3">{TEXT.nutritionalFacts}</h4>
       <Label for="add-food-screen-food-calories">{TEXT.calories}</Label>
       <Input id="add-food-screen-food-calories" size="sm" placeholder="{TEXT.calories}" type="number" step="any"
-             required />
+             required bind:value={calories} />
       <Label for="add-food-screen-food-proteins">{TEXT.proteins}</Label>
       <Input id="add-food-screen-food-proteins" size="sm" placeholder="{TEXT.proteins}" type="number" step="any"
-             required />
+             required bind:value={proteins} />
       <Label for="add-food-screen-food-carbohydrates">{TEXT.carbohydrates}</Label>
       <Input id="add-food-screen-food-carbohydrates" size="sm" placeholder="{TEXT.carbohydrates}" type="number"
-             step="any" required />
+             step="any" required bind:value={carbs} />
       <Label for="add-food-screen-food-fats">{TEXT.fats}</Label>
-      <Input id="add-food-screen-food-fats" size="sm" placeholder="{TEXT.fats}" type="number" step="any" required />
-
-      <div id="add-food-screen-food-microes" class="flex flex-col">
+      <Input id="add-food-screen-food-fats" size="sm" placeholder="{TEXT.fats}" type="number" step="any" required
+             bind:value={fats} />
+      <div id="add-food-screen-food-microes" class="{isShowingMicros ? 'flex flex-col' : 'hidden'}">
         <h4 class="text-2xl font-light mt-3">{TEXT.micronutrients}</h4>
         <div id="add-food-screen-food-additional" />
       </div>
-
-      <ButtonGroup id="add-food-screen-footer" class="justify-center space-x-px">
-        <Button outline color="dark" id="add-food-screen-more" class="w-40">{TEXT.showMore}</Button>
-        <!--                <Button id="add-food-screen-less" class="hidden" outline color="dark">{TEXT.showLess}</Button>-->
-        <Button color="blue" id="add-food-screen-save" class="w-40">{TEXT.save}</Button>
-      </ButtonGroup>
     </div>
+    <!-- Footer -->
+    <ButtonGroup id="add-food-screen-footer" class="justify-center space-x-px mt-auto">
+      <Button outline color="dark" id="add-food-screen-more" class="w-40"
+              on:click={handleExpandOrCollapse}>{expandCollapseText}</Button>
+      <!--                <Button id="add-food-screen-less" class="hidden" outline color="dark">{TEXT.showLess}</Button>-->
+      <Button color="blue" id="add-food-screen-save" class="w-40" on:click={handleSave}>{TEXT.save}</Button>
+    </ButtonGroup>
   </div>
 </div>
