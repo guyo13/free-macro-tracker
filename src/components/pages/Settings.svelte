0a09b7a7b901f0f56442e0fd35589f44288ab3dc
Guy Or <guydude1@gmail.com>
2023-10-29 19:19:29 +0200

    wip Settings page

--- src/components/pages/Settings.svelte
+++ src/components/pages/Settings.svelte
@@ -2,6 +2,8 @@
 All rights reserved. Use of this source code is governed by a GNU GPL
 license that can be found in the LICENSE file. -->
 <script lang="ts">
+  import Button from "flowbite-svelte/Button.svelte";
+  import ButtonGroup from "flowbite-svelte/ButtonGroup.svelte";
   import { onMount } from "svelte";
   import repositoriesProvider from "../../db/repositoriesProvider";
   import unitRepositoryProvider, {
@@ -21,6 +23,13 @@ license that can be found in the LICENSE file. -->
   } from "../../db/userGoalsRepository";
   import { type IExportService } from "../../db/exportService";
   import ExportService from "../../db/exportService";
+
+  const TEXTS = {
+    settings: "Settings",
+    importAndExport: "Import and Export",
+    export: "Export Your Data",
+    import: "Import JSON File",
+  };
 
   let exportService: IExportService;
   onMount(() => {
@@ -61,41 +70,24 @@ license that can be found in the LICENSE file. -->
 
 <div id="settings" class="fmt-tab container-fluid">
   <div id="settings-alerts" class="row justify-content-center" />
+
   <div
-    class="row justify-content-center align-items-center fmt-bg-gainsboro pb-1 pt-1"
+    id="settings-data-control"
+    class="flex-column container flex h-full max-w-4xl"
   >
-    <div class="col-12 col-lg-8 ml-1">
-      <h4 class="fmt-font-2 mb-0">Settings</h4>
-    </div>
-  </div>
-  <div id="settings-data-control" class="row justify-content-center mt-4">
-    <div class="col-12 col-lg-8 mb-4">
-      <h4 class="fmt-font-1-5 mb-0">Import and Export</h4>
-    </div>
-    <div class="col-12 col-lg-8 mb-1">
-      <div
-        id="settings-data-control-export"
-        class="fmt-list-tile fmt-bg-cultured"
-      >
-        <span
-          class="fmt-list-tile-body btn fmt-btn-outline-dark-green d-inline-block"
-          >Export Your Data
-        </span>
-        <span class="fa fa-light fa-download btn fmt-list-tile-trailing" />
-      </div>
-    </div>
-    <div class="col-12 col-lg-8 mb-1">
-      <div
-        id="settings-data-control-import"
-        class="fmt-list-tile fmt-bg-cultured"
-      >
-        <span
-          class="fmt-list-tile-body btn fmt-btn-outline-dark-green d-inline-block"
-          >Import JSON File
-        </span>
-        <span class="fa fa-light fa-file-upload btn fmt-list-tile-trailing" />
-      </div>
-    </div>
+    <span class="mb-3 mt-3 text-2xl font-light">{TEXTS.settings}</span>
+    <h4 class="mb-2 text-xl font-light">{TEXTS.importAndExport}</h4>
+    <ButtonGroup class="w-fit">
+      <Button id="settings-data-control-export">
+        <i class="fa fa-light fa-download mr-2" />
+        {TEXTS.export}
+      </Button>
+      <Button id="settings-data-control-import">
+        <i class="fa fa-light fa-file-upload mr-2" />
+        {TEXTS.import}
+      </Button>
+    </ButtonGroup>
+
     <div
       id="settings-data-control-import-indiv"
       class="input-group col-12 col-lg-8 d-none mb-1"
